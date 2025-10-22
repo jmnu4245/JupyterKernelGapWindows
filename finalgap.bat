6857c69ea2eec095587a78e42dfac40a7ac2f69f
Juan Manuel <juanmanuelfloresdelacruz@gmail.com>
2025-10-22 18:07:09 +0200

    final gap ???

--- finalgap.bat
+++ finalgap.bat
@@ -1,2 +1,2 @@
-wsl -u root bash -c "cd ~ && rm -rf setup-gap* && wget https://raw.githubusercontent.com/jmnu4245/JupyterKernelGapWindows/main/setup-gap.sh && bash ./setup-gap.sh"
+wsl -u root bash -c "cd ~/ && rm -rf setup-gap* && wget https://raw.githubusercontent.com/jmnu4245/JupyterKernelGapWindows/main/setup-gap.sh && bash ./setup-gap.sh"
 pause
